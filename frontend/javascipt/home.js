25c974696623fd12e8379c0cb7f15de62436a365
Aurelie Benhar <aurelie.120183@hotmail.fr>
2021-07-15 12:25:01 +0200

    Bouton déconnexion et responsive page home

--- frontend/javascipt/home.js
+++ frontend/javascipt/home.js
@@ -2,3 +2,10 @@ console.log(sessionStorage);
 if(!sessionStorage.token){
     window.location.href = "login.html";
 }
+
+/**
+ * disconnection
+ */
+const btnDisconnection = document.getElementById("disconnect");
+btnDisconnection.onclick = ()=>{ sessionStorage.clear()};
+
